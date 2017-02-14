de5274f93e0689be24d9307f19d974aab6ba9c47
Vitaly Sharapov <akaredsc2@gmail.com>
2017-02-14 23:54:39 +0200

    Add lesson 07

---
+++ src/main/java/org/vitaly/week03/lesson07/enums/Main.java
@@ -0,0 +1,7 @@
+package org.vitaly.week03.lesson07.enums;
+
+/**
+ * Created by vitaly on 13.02.17.
+ */
+public class Main {
+}
---
+++ src/main/java/org/vitaly/week03/lesson07/factory/Main.java
@@ -0,0 +1,7 @@
+package org.vitaly.week03.lesson07.factory;
+
+/**
+ * Created by vitaly on 13.02.17.
+ */
+public class Main {
+}
